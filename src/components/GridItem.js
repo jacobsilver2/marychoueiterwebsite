9ad7c737d3880a551682a753dc5635426754b610
jacobsilver2 <jacobsilver2@mac.com>
2019-04-23 17:26:25 -0400

    grids and overlays

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -4,11 +4,45 @@ import {spring, Motion} from 'react-motion';
 import styled from 'styled-components';
 
 const StyledGridItem = styled.div`
-  img {
+
+.container {
+    display: table;
+    cursor: pointer;
     width: 100%;
-    object-fit: fill;
-    opacity: 1;
-    background: black;
+  }
+  .subcontainer {
+    position: relative;
+    background-color: #2f3238;
+    overflow:hidden;
+  }
+  .containerImage {
+    position: relative;
+    background-color: #17819c;
+  }
+  .title {
+    text-align: center;
+    font-size: 18px;
+    color: white;
+    position: absolute;
+    top: 5%;
+    left: 5%;
+  }
+  .titleText {
+    font-size: 30px;
+    position: relative;
+  }
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .img {
+    width: 100%;
+    cursor: pointer;
+    position: relative;
+    vertical-align: middle;
   }
 `;
 
@@ -42,10 +76,20 @@ const GridItem = ({slug, imageURL, title}) => {
         return (
           <StyledGridItem onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
             <Link to={`/work/${slug}`}>
-              <>
-                <img style={styleImage} src={imageURL} alt="featured_image"/>
-                <div style={styleSubtitle}><p>{title}</p></div>
-              </>
+              <div className="container">
+                <div className="subcontainer">
+                  <div className="conatainerImage" >
+                    <img className="img" style={styleImage} src={imageURL} alt="featured_image"/>
+                  </div>
+                  <div className="overlay" >
+                    <div className="title" style={styleSubtitle}>
+                      <div className="titleText">
+                        <p dangerouslySetInnerHTML={{__html: title}}/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </Link>
           </StyledGridItem>
         )
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 
 const GridContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat( auto-fit, minmax(250px, 1fr) );
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
   grid-gap: 10px;
   margin: 0 auto;
   max-width: 1620px;
